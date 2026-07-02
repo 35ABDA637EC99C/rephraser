047d73e6157b837dad3f01c3f1498c21516b6cde
35ABDA637EC99C <124869563+35ABDA637EC99C@users.noreply.github.com>
2026-07-02 16:21:53 +0200

    Update rephraser.py

--- rephraser.py
+++ rephraser.py
@@ -216,7 +216,7 @@ if __name__ == '__main__':
                 keyvicompiler.add(' '.join(key), json.dumps(mmodel.chain.model[key]))
             del mmodel
             keyvicompiler.compile()
-            keyvicompiler.WriteToFile(args.model)
+            keyvicompiler.write_to_file(args.model)
             del keyvicompiler
             DCT = keyvi.dictionary.Dictionary(args.model)
     elif args.model != '':
